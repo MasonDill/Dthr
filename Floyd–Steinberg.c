b2b6104d52d7dd0b6660e14eb654229ac9cdeedc
Mason Dill <mdill@cervis.net>
2024-10-04 15:21:27 -0400

    removed omp; fixed file parsing

--- Floyd–Steinberg.c
+++ Floyd–Steinberg.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <omp.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
@@ -16,20 +15,16 @@ int find_closest_palette_color(int oldpixel){
 }
 
 int main(int argc, char *argv[]) {
+    
     //read pgm file
     FILE *f = fopen(argv[1], "r");
-
-    //skip metadata
-    while (fgetc(f) != '\n'); //skip first line P2
-    while (fgetc(f) != '\n'); //skip second line 128 128 dimensions
-    while (fgetc(f) != '\n'); //skip third line 255 width
 
     //read in pixels
     for (int y = 0; y < Y_SIZE; y++) {
         for (int x = 0; x < X_SIZE; x++) {
             pixels[x][y] = 0;
             char c = fgetc(f);
-            while(c != '\n'){
+            while(c != ' ' && c != '\n'){
                 //read in number digit by digit
                 c = atoi(&c);
                 pixels[x][y]*= 10;
@@ -38,46 +33,30 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    omp_lock_t* locks = malloc(sizeof(omp_lock_t) * X_SIZE * Y_SIZE);
-    for (int i = 0; i < X_SIZE * Y_SIZE; i++) {
-        omp_init_lock(&locks[i]);
-    }
-    
-    #pragma omp parallel for schedule(static)
+
     for (int y = 1; y < Y_SIZE; y++) {
         for (int x = 0; x < X_SIZE; x++) {
             uint8_t oldpixel = pixels[x][y];
             uint8_t newpixel = find_closest_palette_color(oldpixel);
             uint8_t quant_error = oldpixel - newpixel;
-            //set_lock will suspend the thread until the lock is available
-            omp_set_lock(&locks[x * Y_SIZE + y]);
             pixels[x][y] = newpixel;
-            omp_unset_lock(&locks[x * Y_SIZE + y]);
 
             if(x < X_SIZE-1){
-                omp_set_lock(&locks[(x+1) * Y_SIZE + y]);
                 pixels[x + 1][y] += quant_error * 7 / 16;
-                omp_unset_lock(&locks[(x+1) * Y_SIZE + y]);
             }
  
             if(x > 0 && y < Y_SIZE - 1){
-                omp_set_lock(&locks[(x-1) * Y_SIZE + y + 1]);
                 pixels[x - 1][y + 1] += quant_error * 3 / 16;
-                omp_unset_lock(&locks[(x-1) * Y_SIZE + y + 1]);
             }
  
             if(y < Y_SIZE - 1){
-                omp_set_lock(&locks[x * Y_SIZE + y + 1]);
                 pixels[x][y + 1] += quant_error * 5 / 16;
-                omp_unset_lock(&locks[x * Y_SIZE + y + 1]);
             }
    
             if(x < X_SIZE - 1 && y < Y_SIZE - 1){
-                omp_set_lock(&locks[(x+1) * Y_SIZE + y + 1]);
                 pixels[x + 1][y + 1] += quant_error * 1 / 16;
-                omp_unset_lock(&locks[(x+1) * Y_SIZE + y + 1]);
             }
-        } 
+        }
     }
 
     //write dithered image to file
