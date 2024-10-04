9d7f1c9e1f051c35e172cfd135028cf2ad23f7a8
Mason Dill <mdill@cervis.net>
2024-10-04 16:44:10 -0400

    reodered pixels

--- Floyd–Steinberg.c
+++ Floyd–Steinberg.c
@@ -44,6 +44,23 @@ int* sanityCheckArgs(int argc, char *argv[]){
     return 0;
 }
 
+uint8_t** readImage(char* inputFile, int x_size, int y_size){
+    uint8_t** pixels = (uint8_t**)malloc(y_size * sizeof(uint8_t*));
+
+    FILE *f = fopen(inputFile, "r");
+    for (int y = 0; y < y_size; y++) {
+        pixels[y] = (uint8_t*)malloc(x_size * sizeof(uint8_t));
+        for (int x = 0; x < x_size; x++) {
+            int pixel;
+            fscanf(f, "%d", &pixel);
+            pixels[y][x] = pixel;
+        }
+    }
+    fclose(f);
+
+    return pixels;
+}
+
 int main(int argc, char *argv[]) {
     sanityCheckArgs(argc, argv);
 
@@ -57,56 +74,30 @@ int main(int argc, char *argv[]) {
         passes = atoi(argv[PASSES_IDX]);
     }
 
-    uint8_t pixels[x_size][y_size];
-
-    //read pgm file
-    FILE *f = fopen(argv[1], "r");
-
-    //read in pixels
-    for (int y = 0; y < y_size; y++) {
-        for (int x = 0; x < x_size; x++) {
-            pixels[x][y] = 0;
-            char c = fgetc(f);
-
-            int pixelCounter = 0;
-            while(c != ' ' && c != '\n'){
-                //read in number digit by digit
-                c = atoi(&c);
-                pixels[x][y]*= 10;
-                pixels[x][y] += c;
-                c = fgetc(f);
-
-                if(pixelCounter > 3 || pixels[x][y] > 255){
-                    printf("Invalid pixel value. Please make sure the provided file was given in the correct format.\n");
-                    exit(1);
-                }
-            }
-        }
-    }
-    fclose(f);
+    uint8_t** pixels = readImage(inputFile, x_size, y_size);
 
     for(int p = 0; p < passes; p++){
         for (int y = 1; y < y_size; y++) {
             for (int x = 0; x < x_size; x++) {
-                uint8_t oldpixel = pixels[x][y];
+                uint8_t oldpixel = pixels[y][x];
                 uint8_t newpixel = find_closest_palette_color(oldpixel);
                 uint8_t quant_error = oldpixel - newpixel;
-                pixels[x][y] = newpixel;
+                pixels[y][x] = newpixel;
 
                 if(x < x_size-1){
-                    pixels[x + 1][y] += quant_error * 7 / 16;
+                    pixels[y][x + 1] += quant_error * 7 / 16;
                 }
     
                 if(x > 0 && y < y_size - 1){
-                    pixels[x - 1][y + 1] += quant_error * 3 / 16;
+                    pixels[y + 1][x - 1] += quant_error * 3 / 16;
                 }
     
                 if(y < y_size - 1){
-                    pixels[x][y + 1] += quant_error * 5 / 16;
+                    pixels[y + 1][x] += quant_error * 5 / 16;
                 }
     
                 if(x < x_size - 1 && y < y_size - 1){
-                    pixels[x + 1][y + 1] += quant_error * 1 / 16;
+                    pixels[y + 1][x + 1] += quant_error * 1 / 16;
                 }
             }
         }
@@ -114,7 +105,7 @@ int main(int argc, char *argv[]) {
         //repolate image
         for (int y = 0; y < y_size; y++) {
             for (int x = 0; x < x_size; x++) {
-                pixels[x][y] += 255;
+                pixels[y][x] += 255;
             }
         }
     }
@@ -126,7 +117,7 @@ int main(int argc, char *argv[]) {
     fprintf(f5, "255\n"); //255 bit depth
     for (int y = 0; y < y_size; y++) {
         for (int x = 0; x < x_size; x++) {
-            fprintf(f5, "%d\n", pixels[x][y]);
+            fprintf(f5, "%d\n", pixels[y][x]);
         }
     }
     fclose(f5);
