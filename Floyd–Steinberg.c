18e1fa3297e7e20db4fc257791482b4885c2178d
Mason Dill <mdill@cervis.net>
2024-10-04 15:59:50 -0400

    configurable argument size

--- Floyd–Steinberg.c
+++ Floyd–Steinberg.c
@@ -4,10 +4,10 @@
 #include <math.h>
 
 //change for different image sizes
-#define X_SIZE 128
-#define Y_SIZE 128
-
-uint8_t pixels[X_SIZE][Y_SIZE];
+#define INPUT_FILE_IDX 1
+#define X_SIZE_IDX 2
+#define Y_SIZE_IDX 3
+#define OUTPUT_FILE_IDX 4
 
 int find_closest_palette_color(int oldpixel){
     //converts from 0-255 grayscale to 0-1, black-white
@@ -15,17 +15,27 @@ int find_closest_palette_color(int oldpixel){
 }
 
 int* sanityCheckArgs(int argc, char *argv[]){
-    if(argc != 3){
-        printf("Usage: ./Floyd-Steinberg <input.data> <output.pgm>\n");
+    char input[] = "<input.data>";
+    char output[] = "<output.pgm>";
+    char x_size[] = "<x_size>";
+    char y_size[] = "<y_size>";
+
+    char** args = (char**)malloc(4 * sizeof(char*));
+    args[INPUT_FILE_IDX] = input;
+    args[X_SIZE_IDX] = x_size;
+    args[Y_SIZE_IDX] = y_size;
+    args[OUTPUT_FILE_IDX] = output;
+
+    if(argc != 5){
+        printf("Usage: %s %s %s %s %s\n", argv[0], input, x_size, y_size, output);
         exit(1);
     }
 
-    FILE *f = fopen(argv[1], "r");
+    FILE *f = fopen(argv[INPUT_FILE_IDX], "r");
     if(f == NULL){
         printf("File %s does not exist\n", argv[1]);
         exit(1);
     }
-
     fclose(f);
 
     return 0;
@@ -34,12 +44,24 @@ int* sanityCheckArgs(int argc, char *argv[]){
 int main(int argc, char *argv[]) {
     sanityCheckArgs(argc, argv);
 
+    char* inputFile = argv[INPUT_FILE_IDX];
+
+    char* x_size_str = argv[X_SIZE_IDX];
+    int x_size = atoi(x_size_str);
+    
+    char* outputFile = argv[OUTPUT_FILE_IDX];
+
+    char* y_size_str = argv[Y_SIZE_IDX];
+    int y_size = atoi(y_size_str);
+
+    uint8_t pixels[x_size][y_size];
+
     //read pgm file
     FILE *f = fopen(argv[1], "r");
 
     //read in pixels
-    for (int y = 0; y < Y_SIZE; y++) {
-        for (int x = 0; x < X_SIZE; x++) {
+    for (int y = 0; y < y_size; y++) {
+        for (int x = 0; x < x_size; x++) {
             pixels[x][y] = 0;
             char c = fgetc(f);
 
@@ -60,37 +82,37 @@ int main(int argc, char *argv[]) {
     }
     fclose(f);
 
-    for (int y = 1; y < Y_SIZE; y++) {
-        for (int x = 0; x < X_SIZE; x++) {
+    for (int y = 1; y < y_size; y++) {
+        for (int x = 0; x < x_size; x++) {
             uint8_t oldpixel = pixels[x][y];
             uint8_t newpixel = find_closest_palette_color(oldpixel);
             uint8_t quant_error = oldpixel - newpixel;
             pixels[x][y] = newpixel;
 
-            if(x < X_SIZE-1){
+            if(x < x_size-1){
                 pixels[x + 1][y] += quant_error * 7 / 16;
             }
  
-            if(x > 0 && y < Y_SIZE - 1){
+            if(x > 0 && y < y_size - 1){
                 pixels[x - 1][y + 1] += quant_error * 3 / 16;
             }
  
-            if(y < Y_SIZE - 1){
+            if(y < y_size - 1){
                 pixels[x][y + 1] += quant_error * 5 / 16;
             }
    
-            if(x < X_SIZE - 1 && y < Y_SIZE - 1){
+            if(x < x_size - 1 && y < y_size - 1){
                 pixels[x + 1][y + 1] += quant_error * 1 / 16;
             }
         }
     }
 
     //write dithered image to file
-    FILE *f5 = fopen(argv[2], "w");
-    fprintf(f5, "P2\n%d %d\n", X_SIZE, Y_SIZE); //P2 format, dimensions
+    FILE *f5 = fopen(argv[OUTPUT_FILE_IDX], "w");
+    fprintf(f5, "P2\n%d %d\n", x_size, y_size); //P2 format, dimensions
     fprintf(f5, "255\n"); //255 bit depth
-    for (int y = 0; y < Y_SIZE; y++) {
-        for (int x = 0; x < X_SIZE; x++) {
+    for (int y = 0; y < y_size; y++) {
+        for (int x = 0; x < x_size; x++) {
             //repolate
             pixels[x][y] += 255;
 
