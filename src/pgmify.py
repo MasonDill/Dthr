f12909eafa13155b9dc8da5325deab1a77638488
Mason Dill <mason77804@gmail.com>
2024-10-07 21:19:55 -0400

    removed extraneous functions from pgmify.py

--- src/pgmify.py
+++ src/pgmify.py
@@ -1,59 +1,31 @@
-import numpy as np
-from sys import argv
-
-def byteArrToIntArr(byteArr):
-    intArr = []
-    for r in byteArr:
-        intArr.append([])
-        for c in r:
-            intArr[-1].append(int.from_bytes(c, "little"))
-    return intArr
-
-def intToByteArr(intArr):
-    byteArr = []
-    for r in intArr:
-        byteArr.append([])
-        for c in r:
-            n = int(c)
-            byteArr[-1].append(n.to_bytes(1, 'little'))
-
-    return byteArr
-
-def scrapePgmMetadata(name):
-    pgmf = open(name, 'rb')
-    pgmf.readline()
-    (width, height) = [int(i) for i in pgmf.readline().split()]
-    pgmf.close()
-    return width, height
-
 #turns a .pgm into a 2D array of bytes
 def readpgm(name):
     pgmf = open(name, 'rb')
-    pgmf.readline()
+    pgm_header = pgmf.readline()
+    if pgm_header not in [b'P5\n', b'P2\n']:
+        raise ValueError("Not a valid PGM file")
+
     (width, height) = [int(i) for i in pgmf.readline().split()]
     #depth = int(pgmf.readline())
-    bytevals = []
+    pixels = []
 
     for _ in range(0, height):
         row = []
         for _ in range(0, width):
-            row.append((pgmf.read(1)))
-        bytevals.append(row)
+            byte_val = pgmf.read(1)
+            row.append(int.from_bytes(byte_val, "little"))
+        pixels.append(row)
     pgmf.close()
 
-    return bytevals
+    return pixels, width, height
 
 def pgmToDataFile(input_file, output_file):
-    img = pgmToArr(input_file)
-    np.savetxt(output_file, img, fmt="%d")
-
-def pgmToArr(name):
-    img = readpgm(name)
-    img = byteArrToIntArr(img)
-    img = np.array(img)
-    return img
+    from numpy import savetxt as np_savetxt
+    img, _, _ = readpgm(input_file)
+    np_savetxt(output_file, img, fmt="%d")
 
 if __name__ == "__main__":
+    from sys import argv
     input_file = argv[1]
     output_file = argv[2]
     pgmToDataFile(input_file, output_file)
